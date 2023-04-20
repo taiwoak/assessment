cfdbbde2dac33dcc511e2cb8eefc32546c3b57a2
taiwoak <taiwoakerele98@gmail.com>
2023-04-20 10:55:25 +0100

    added the mobile view of the homepage

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+function myMenu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myCloseMenu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
